f7016a250c866e47519f478f54ca95c46d664afd
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-07-11 15:57:36 +0200

    Testing github actions

--- client/tests/example.spec.ts
+++ client/tests/example.spec.ts
@@ -1,18 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle('CMS scaffold client');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('has description', async ({ page }) => {
+  await page.goto('/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  const metaDescription = await page.locator('meta[name="description"]');
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  await expect(metaDescription).toHaveAttribute('content', 'CMS scaffold client description');
 });
