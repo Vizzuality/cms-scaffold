d5642f14bb7b059f6a095fa72d114704ec25c060
Agnieszka Figiel <agnieszka.figiel@vizzuality.com>
2023-11-22 15:51:34 +0100

    GCP infrastructure

---
+++ infrastructure/gcp/base/modules/secret_value/outputs.tf
@@ -0,0 +1,15 @@
+output "secret_name" {
+  value = google_secret_manager_secret.secret.secret_id
+}
+
+output "secret_id" {
+  value = google_secret_manager_secret.secret.id
+}
+
+output "secret_value" {
+  value = google_secret_manager_secret_version.backend_app_secret.secret_data
+}
+
+output "latest_version" {
+  value = data.google_secret_manager_secret_version.latest.version
+}
---
+++ infrastructure/gcp/state/vars/terraform.tfvars
@@ -0,0 +1,3 @@
+gcp_region     = "us-east1"
+gcp_project_id = "project_id
+bucket_name    = "prefix-tf-state"
